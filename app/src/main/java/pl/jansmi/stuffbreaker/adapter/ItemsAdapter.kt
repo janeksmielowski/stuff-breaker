85f23718be964859bbfb59599977c2add4b57ba3
jansmi <janek.smielowski@gmail.com>
2020-03-29 17:51:30 +0200

    Items adapter fixes

--- app/src/main/java/pl/jansmi/stuffbreaker/adapter/ItemsAdapter.kt
+++ app/src/main/java/pl/jansmi/stuffbreaker/adapter/ItemsAdapter.kt
@@ -1,11 +1,16 @@
 package pl.jansmi.stuffbreaker.adapter
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.view.marginLeft
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.runBlocking
+import pl.jansmi.stuffbreaker.EditItemActivity
 import pl.jansmi.stuffbreaker.MainActivity
 import pl.jansmi.stuffbreaker.R
 import pl.jansmi.stuffbreaker.database.entity.Box
@@ -19,27 +24,46 @@ class ItemsAdapter(val box: Box): RecyclerView.Adapter<ItemsAdapter.ItemHolder>(
     // TODO: desc , tags & image
     class ItemHolder(view: View): RecyclerView.ViewHolder(view) {
         private var title: TextView
+        private var desc: TextView
+        private var image: ImageView
 
         init {
             title = view.findViewById(R.id.title)
+            desc = view.findViewById(R.id.desc)
+            image = view.findViewById(R.id.image)
         }
 
         fun bindBox(box: Box) {
             title.text = box.name
+            desc.text = box.desc
+
+            itemView.setOnClickListener {
+
+            }
         }
 
         fun bindItem(item: Item) {
-            // TODO: kind of differentiation for box and item
             title.text = item.name
+            desc.text = item.desc
+            // TODO: image.setImageResource()
+            // custom icon if no image provided
+            image.setImageResource(R.drawable.ic_baseline_crop_square_64)
+            image.marginLeft.plus(10)
+
+            itemView.setOnClickListener {
+                Log.i("OKOK", "okok")
+                val intent = Intent(itemView.context, EditItemActivity::class.java)
+                intent.putExtra("box", item.boxId)
+                intent.putExtra("item", item.id)
+                itemView.context.startActivity(intent)
+            }
         }
 
     }
 
     init {
-        runBlocking {
-            boxes = MainActivity.database.boxes().findAllBoxesByParentId(box.id)
-            items = MainActivity.database.items().findAllItemsByBoxId(box.id)
-        }
+        boxes = MainActivity.database.boxes().findAllBoxesByParentId(box.id)
+        items = MainActivity.database.items().findAllItemsByBoxId(box.id)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
