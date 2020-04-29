1848f7261982b78bac9684746dd7999cd247b258
jansmi <janek.smielowski@gmail.com>
2020-04-29 19:55:26 +0200

    Added image path to Item entity

--- app/src/main/java/pl/jansmi/stuffbreaker/database/entity/Item.kt
+++ app/src/main/java/pl/jansmi/stuffbreaker/database/entity/Item.kt
@@ -11,8 +11,8 @@ data class Item(
     @ColumnInfo(name = "name") var name: String,
     @ColumnInfo(name = "desc") var desc: String?,
     @ColumnInfo(name = "box_id") var boxId: Int,
-    @ColumnInfo(name = "qr_code") var qrCode: String?
-    //@ColumnInfo(name = "image") var image: Image
+    @ColumnInfo(name = "qr_code") var qrCode: String?,
+    @ColumnInfo(name = "image") var imagePath: String?
 ) {
     @PrimaryKey(autoGenerate = true) var id: Int = 0
 }
