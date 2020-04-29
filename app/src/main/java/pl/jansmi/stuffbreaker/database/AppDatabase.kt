069d23c2b8f670bf78e66107c1aaa01dc4ece8f6
jansmi <janek.smielowski@gmail.com>
2020-04-29 19:53:58 +0200

    Global database companion

--- app/src/main/java/pl/jansmi/stuffbreaker/database/AppDatabase.kt
+++ app/src/main/java/pl/jansmi/stuffbreaker/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package pl.jansmi.stuffbreaker.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import pl.jansmi.stuffbreaker.database.dao.BoxDao
 import pl.jansmi.stuffbreaker.database.dao.ItemDao
@@ -11,4 +13,25 @@ import pl.jansmi.stuffbreaker.database.entity.Item
 abstract class AppDatabase: RoomDatabase() {
     abstract fun boxes(): BoxDao
     abstract fun items(): ItemDao
+
+    companion object {
+        private val DB_NAME = "database.db";
+        private var db: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            if (db == null)
+                db = buildDatabaseInstance(context)
+            return db!!;
+        }
+
+        fun buildDatabaseInstance(context: Context): AppDatabase {
+            return Room
+                .databaseBuilder(context, AppDatabase::class.java, DB_NAME)
+                .allowMainThreadQueries()
+                .build();
+        }
+    }
+
+
+
 }
